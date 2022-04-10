8351ec1313089095a3b90522cf584262283e2b3d
dlp-buyankhishig <buyankhishig@tapatrip.com>
2022-04-10 19:29:53 +0800

    Шинэ үгийн хуудасанд бүлэг сонгох хэсэг оруулсан

--- lib/screens/vocabulary_home_screen/vocabulary_home_screen.dart
+++ lib/screens/vocabulary_home_screen/vocabulary_home_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:husky/components/my_app_bar.dart';
 import 'package:husky/global/style.dart';
 import 'package:husky/screens/vocabulary_home_screen/components/vocabulary_levels.dart';
+import 'package:husky/screens/vocabulary_home_screen/components/vocabulary_sub_levels.dart';
 
 class VocabularyHomeScreen extends StatefulWidget {
   const VocabularyHomeScreen();
@@ -14,11 +15,12 @@ class _VocabularyHomeScreenState extends State<VocabularyHomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: myAppBar(title: "Үг цээжлэх"),
-      backgroundColor: Styles.greyColor,
-      body: Column(
-        children: [SingleChildScrollView(scrollDirection: Axis.horizontal, child: VocabularyLevels())],
-      ),
-    );
+        appBar: myAppBar(title: "Үг цээжлэх"),
+        backgroundColor: Styles.greyColor,
+        body: SingleChildScrollView(
+          child: Column(
+            children: [SingleChildScrollView(scrollDirection: Axis.horizontal, child: VocabularyLevels()), VocabularySubLevels()],
+          ),
+        ));
   }
 }
