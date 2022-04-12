a8cfc2588af60124f78dfcca119dd7790bdf3339
dlp-buyankhishig <buyankhishig@tapatrip.com>
2022-04-12 08:53:43 +0800

    vocabulary detail screen

--- lib/app_router.dart
+++ lib/app_router.dart
@@ -3,7 +3,9 @@ import 'package:husky/components/my_app_bar.dart';
 import 'package:husky/screens/grammar_home_screen/grammar_home_screen.dart';
 import 'package:husky/screens/mockTest_home_screen/mockTest_home_screen.dart';
 import 'package:husky/screens/studyChina_home_screen/study_china_home_screen.dart';
+import 'package:husky/screens/vocabulary_home_screen/vocabulary_detail_screen.dart';
 import 'package:husky/screens/vocabulary_home_screen/vocabulary_home_screen.dart';
+import 'package:husky/screens/vocabulary_home_screen/vocabulary_list_screen.dart';
 import 'screens/home_screen/home_screen.dart';
 
 class AppRouter {
@@ -13,6 +15,10 @@ class AppRouter {
         return MaterialPageRoute(builder: (_) => HomeScreen());
       case '/vocabulary_home_screen':
         return MaterialPageRoute(builder: (_) => VocabularyHomeScreen());
+      case '/vocabulary_list_screen':
+        return MaterialPageRoute(builder: (_) => VocabularyListScreen(args: settings.arguments));
+      case '/vocabulary_detail_screen':
+        return MaterialPageRoute(builder: (_) => VocabularyDetailScreen(args: settings.arguments));
       case '/grammar_home_screen':
         return MaterialPageRoute(builder: (_) => GrammarHomeScreen());
       case '/mockTest_home_screen':
--- lib/screens/vocabulary_home_screen/components/vocabulary_sub_level.dart
+++ lib/screens/vocabulary_home_screen/components/vocabulary_sub_level.dart
@@ -9,36 +9,41 @@ class VocabularySubLevel extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-        margin: EdgeInsets.all(6),
-        padding: EdgeInsets.all(10),
-        decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(5),
-            color: Styles.whiteColor,
-            boxShadow: [BoxShadow(color: Styles.textColor10, offset: Offset(2, 4), spreadRadius: 0, blurRadius: 8)]),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            SizedBox(),
-            Column(
-              children: [
-                Container(
-                  decoration: BoxDecoration(color: Styles.baseColor, borderRadius: BorderRadius.circular(10)),
-                  padding: EdgeInsets.symmetric(vertical: 5, horizontal: 10),
-                  child: MyText.medium(
-                    "10/100",
-                    textColor: Styles.whiteColor,
-                    fontWeight: Styles.wBold,
+    return InkWell(
+      onTap: () {
+        Navigator.pushNamed(context, "/vocabulary_list_screen");
+      },
+      child: Container(
+          margin: EdgeInsets.all(6),
+          padding: EdgeInsets.all(10),
+          decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(5),
+              color: Styles.whiteColor,
+              boxShadow: [BoxShadow(color: Styles.textColor10, offset: Offset(2, 4), spreadRadius: 0, blurRadius: 8)]),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              SizedBox(),
+              Column(
+                children: [
+                  Container(
+                    decoration: BoxDecoration(color: Styles.baseColor, borderRadius: BorderRadius.circular(10)),
+                    padding: EdgeInsets.symmetric(vertical: 5, horizontal: 10),
+                    child: MyText.medium(
+                      "10/100",
+                      textColor: Styles.whiteColor,
+                      fontWeight: Styles.wBold,
+                    ),
                   ),
-                ),
-                SizedBox(height: 3),
-                MyText.medium(
-                  "Бүлэг $index",
-                  textColor: Styles.textColor,
-                ),
-              ],
-            )
-          ],
-        ));
+                  SizedBox(height: 3),
+                  MyText.medium(
+                    "Бүлэг $index",
+                    textColor: Styles.textColor,
+                  ),
+                ],
+              )
+            ],
+          )),
+    );
   }
 }
