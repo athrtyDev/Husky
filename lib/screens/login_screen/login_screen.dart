b9a8bceadbeb5bc0c0a86e7d72de03130f29ec6b
dlp-buyankhishig <buyankhishig@tapatrip.com>
2022-06-14 07:10:55 +0800

    google login success

--- lib/screens/login_screen/login_screen.dart
+++ lib/screens/login_screen/login_screen.dart
@@ -1,8 +1,10 @@
 import 'package:diyi/components/my_text.dart';
 import 'package:diyi/global/global.dart';
+import 'package:diyi/utils/base_functions.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_facebook_auth/flutter_facebook_auth.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({Key key}) : super(key: key);
@@ -43,7 +45,29 @@ class _LoginScreenState extends State<LoginScreen> {
                     ),
                   ),
                 ),
-              )
+              ),
+              Container(
+                margin: EdgeInsets.all(30),
+                child: InkWell(
+                  onTap: () {
+                    signInWithGoogle();
+                  },
+                  child: Container(
+                    padding: EdgeInsets.all(30),
+                    decoration: BoxDecoration(color: Styles.baseColor, borderRadius: BorderRadius.circular(20)),
+                    child: Row(
+                      children: [
+                        Icon(Icons.phone),
+                        SizedBox(width: 10),
+                        MyText.medium(
+                          "Google нэвтрэх",
+                          textColor: Styles.whiteColor,
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+              ),
             ],
           ),
         ),
@@ -87,6 +111,30 @@ class _LoginScreenState extends State<LoginScreen> {
       final OAuthCredential credential = FacebookAuthProvider.credential(result.accessToken.token);
       // Once signed in, return the UserCredential
       return await FirebaseAuth.instance.signInWithCredential(credential);
+    }
+    return null;
+  }
+
+  Future<UserCredential> signInWithGoogle() async {
+    // Configure SHA-1 certificate and download google-services.json in project settings
+
+    final GoogleSignInAccount googleUser = await GoogleSignIn().signIn();
+    final GoogleSignInAuthentication googleAuth = await googleUser.authentication;
+    print("helllooo:: ${googleAuth.accessToken}");
+    print("helllooo:: ${googleUser.email}");
+
+    if (googleAuth.accessToken != null && googleAuth.idToken != null) {
+      // Create a credential from the access token
+      final OAuthCredential credential =
+          GoogleAuthProvider.credential(accessToken: googleAuth.accessToken, idToken: googleAuth.idToken);
+      // Once signed in, return the UserCredential
+      UserCredential userCredential = await FirebaseAuth.instance.signInWithCredential(credential);
+      if (userCredential.additionalUserInfo.isNewUser) {
+        // todo do smthg
+        baseFunctions.showSuccessToasts("google sign up success");
+      } else {
+        baseFunctions.showSuccessToasts("google login success");
+      }
     }
     return null;
   }
